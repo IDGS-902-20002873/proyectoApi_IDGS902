9bdc8adbc88eda9fee0e4fc98c38b0d5300b797e
IDGS-902-20002873 <20002873@alumnos.utleon.edu.mx>
2023-07-10 23:09:11 -0600

    RCTG 10/07/2023

---
+++ src/app/proyecto-api.service.ts
@@ -0,0 +1,53 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AlumnosUtl } from './interfaces/utl.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProyectoApiService {
+
+  private _alumnosUtl:AlumnosUtl[]=[
+    {
+      id:1234,
+      nombre:'pedro',
+      edad:23,
+      correo: 'pedro@gmail.com',
+ 
+    },
+    {
+      id:772,
+      nombre:'Paulina',
+      edad:23,
+      correo: 'paulina@gmail.com',
+    },
+ 
+    {
+      id:22,
+      nombre:'Dario',
+      edad:23,
+      correo: 'dario@gmail.com',
+ 
+    },
+  ]
+ 
+  constructor(private http:HttpClient) { }
+ 
+get alumnos():AlumnosUtl[]{
+  return[...this._alumnosUtl]
+}
+ 
+public getAlumnos():Observable<AlumnosUtl[]>{
+  return this.http.get<AlumnosUtl[]>('https://localhost:7192/api/Grupos')
+}
+ 
+agregarNuevoAlumno(datos:AlumnosUtl){
+  return this.http.post('https://localhost:7192/api/Grupos',datos)
+}
+
+editarAlumno(datos:AlumnosUtl){
+  return this.http.put('https://localhost:7192/api/Grupos',(datos.id, datos))
+}
+
+}
